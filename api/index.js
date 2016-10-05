f883b1c17ad9573c3b6c0e7a899f2254dd58293d
Jordi Moraleda <moraleda@uniclau.com>
2016-10-05 03:09:07 +0200

    Initial import

---
+++ api/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// API ROUTINES
+// var customerRouter = require('./customer');
+var postRouter = require('./post');
+var productRouter = require('./product');
+// var purchaseRouter = require('./purchase');
+var shopRouter = require('./shop');
+
+var summaryRouter = require('./summary');
+
+// ROUTER
+var router = express.Router();
+router.use('/api', bodyParser.json({limit: '10mb'}));
+
+// ROUTES
+// router.use('/api', customerRouter);
+router.use('/api', postRouter);
+router.use('/api', productRouter);
+// router.use('/api', purchaseRouter);
+router.use('/api', shopRouter);
+
+router.use('/api', summaryRouter);
+
+// Error handling
+router.use('/api', function(err, req, res, next){
+	if(!err) return res.send('');
+	res.status(500).send({error: err.message || err});
+});
+
+// EXPORT
+module.exports = router;
---
+++ src/directives/index.js
@@ -0,0 +1,10 @@
+module.exports = function(app){
+
+	// app.directive('purchasesMarkProcessing', require('./purchases-mark-processing'));
+	// app.directive('purchasesMarkDone', require('./purchases-mark-done'));
+	// app.directive('purchasesGenerateWaybills', require('./purchases-generate-waybills'));
+
+	// app.directive('purchaseMarkProcessing', require('./purchase-mark-processing'));
+	// app.directive('purchaseMarkDone', require('./purchase-mark-done'));
+
+};
